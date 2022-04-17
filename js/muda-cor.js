ca390e09e721a40b65c28885964cb54d5fe7afb0
Wesley de Lima Marques <66307832+wesleyLM@users.noreply.github.com>
2022-04-17 18:15:47 -0300

    foi feito modificação no html para suporte para telas responsivas e melhorado o código do muda-cor.js

--- js/muda-cor.js
+++ js/muda-cor.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
     //foi selecionado o input color com div que mudará de cor 
     let inputColor = document.getElementById('personalizacao_cor')
     let divColor = document.querySelector('.editor_codigo-cor')
@@ -7,14 +7,12 @@
     divColor.style.backgroundColor = inputColor.value
 
     //função que mudará a cor da div pelo o input
-    function mudaCorDiv(){
+    function mudaCorDiv() {
         divColor.style.backgroundColor = this.value
     }
 
     //escuta o evento de muda a cor do input
     inputColor.addEventListener('input', mudaCorDiv)
-
-    
 })()
 
 
